8c34b76bbac84393a928511bea46f9ff96c99a85
Elisabeth Petit - Bois <epetitbo@students.kennesaw.edu>
2018-10-21 01:16:20 -0400

    Added widgets to dashboard

--- js/app.js
+++ js/app.js
@@ -1,48 +1,20 @@
 $("document").ready(function() {
 
-
-    /*var data = (function () {
-        var json = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': '../json/cumulativeLineData.json',
-            'dataType': "json",
-            'success': function (data) {
-                json = data;
-            }
-        });
-        return json;
-    })(); 
-
-    nv.addGraph(function() {
-        var chart = nv.models.cumulativeLineChart()
-                    .x(function(d) { return d[0] })
-                    .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
-                    .color(d3.scale.category10().range())
-                    .useInteractiveGuideline(true)
-                    .showLegend(false)
-                    ;
-    
-        chart.xAxis
-            .tickValues([1078030800000,1122782400000,1167541200000,1251691200000])
-            .tickFormat(function(d) {
-                return d3.time.format('%x')(new Date(d))
-            });
-    
-        chart.yAxis
-            .tickFormat(d3.format(',.1%'));
-    
-        d3.select('#graph-svg svg')
-            .datum(data)
-            .call(chart);
-    
-        //TODO: Figure out a good way to do this automatically
-        nv.utils.windowResize(chart.update);
-    
-        return chart;
+    dragula([document.querySelector('#left-grid'), document.querySelector('#right-grid')]);
+    /*dragula([document.querySelector('#grid')], {
+        moves: function(el, container, handle) {
+          return !handle.classList.contains('.widget');
+        }
     });*/
 
-    dragula([document.querySelector('#left-grid'), document.querySelector('#right-grid')]);
+    $('.movers').owlCarousel({
+        items: 4,
+        nav: true
+    });
+    /*$('.news').owlCarousel({
+        items: 1,
+        nav: true,
+        autoWidth: true
+    });*/
 
 })
